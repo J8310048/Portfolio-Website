3d2c48feb542bc762de30e4ba36ffce8a2066719
Joshua Brito-Infante <the.business.of.joshua@gmail.com>
2025-04-13 14:25:23 -0400

    moving away from creating post requests from my contact form to my sql database. Opting in for nodemailer instead. Put all files in a public folder except server.js, package.json, pakckage-lock.json, .env, and .gitignore.

---
+++ public/script.js
@@ -0,0 +1,143 @@
+const express = require('express');
+const { app } = require('../server');
+
+// NAV MENU CODE START
+const hamburgerMenu = document.querySelector(".hamburgerMenu");
+const navLinks = document.querySelector(".navLinks");
+
+
+// below connects the hamburger menu and navlinks together by adding an event listener to the menu and having the click event display the links by using the "show" class in the CSS file
+hamburgerMenu.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+});
+
+// NAV MENU CODE END
+
+
+
+// PROJECT CAROUSEL SLIDER CODE START
+const sliderItems = document.querySelectorAll(".projectItem")
+console.log(sliderItems)
+
+
+// // below sets up how the slider will look like by default
+
+// let currentIndex = 0;
+
+// function showProject(current) {
+//     sliderItems.forEach((item, index) => {
+//         if (index === current) {
+//             item.style.display = "block"; // this shows every project item that's equal the first index position in the array
+//         } else {
+//             item.style.display = "none"; // this hides every project item not equal the first index position in the array
+//         }
+//     });
+// }
+
+// showProject(currentIndex)
+
+
+
+// const prevBtn = document.querySelector(".previous")
+// console.log(prevBtn)
+// const nextBtn = document.querySelector(".next")
+// console.log(nextBtn)
+
+// prevBtn.addEventListener("click", () => {
+//     currentIndex = currentIndex - 1;
+//     if (currentIndex < 0) {
+//         currentIndex = sliderItems.length - 1 // this loops to the last project
+//     }
+//     console.log("This clicks!")
+//     showProject(currentIndex)
+// })
+
+// nextBtn.addEventListener("click", () => {
+//     currentIndex = currentIndex + 1;
+//     if (currentIndex >= sliderItems.length) {
+//         currentIndex = 0 // this loops back to the first project
+//     }
+//     console.log("This clicks too!")
+//     showProject(currentIndex)
+// })
+
+// PROJECT CAROUSEL SLIDER CODE END
+
+
+
+
+// class ProjectCarousel {
+//     constructor(item, index, currentIndex) {
+//         this.item = item;
+//         this.index = index;
+//         this.currentIndex = currentIndex;
+//     }
+
+//     showProject(currentIndex) {
+
+//     }
+
+//     previousBtn() {
+//         this.currentIndex = 0
+//         addEventListener("click", () => {
+//             currentIndex = currentIndex - 1
+//             if (currentIndex < 0) {
+//                 currentIndex = sliderItems.length - 1
+//             }
+//         })
+//     }
+
+//     nextBtn() {
+//         this.currentIndex = 0
+//         addEventListener("click", () => {
+//             currentIndex = currentIndex + 1
+//             if (currentIndex >= sliderItems.length) {
+//                 currentIndex = 0
+//             }
+//         })
+//     }
+// }
+
+// CLASS PROJECT CAROUSEL SLIDER START
+
+
+// class ProjectCarousel {
+
+//     constructor(container, previousSelection, nextSelection) {
+//         this.container = container;
+//         this.previousSelection = previousSelection;
+//         this.nextSelection = nextSelection;
+
+
+//     }
+
+
+// }
+
+// // Instantiate the class for a project carousel
+// const projectSlider = new ProjectCarousel(".projectSection", ".previous", ".next");
+
+// CLASS PROJECT CAROUSEL SLIDER END
+
+const contactForm = document.getElementById("#contactForm")
+contactForm.addEventListener("submit", (Event) => {
+    Event.preventDefault //this stops the from reloading itself after clicking submit
+})
+
+
+const formInputs = Event.target
+const formCapture = new URLSearchParams(new formCapture(formInputs))
+
+fetch("http://localhost:3000/contact", {
+    method: "POST",
+    headers: {
+        "content-type": "application/x-www-form-urlencoded"
+    },
+    body: formCapture.toString()
+})
+    .then(res => res.text())
+    .then(data => {
+        console.log("Here's your response from the server: ", data)
+        alert("Form submission successful!")
+    })
+    .catch(err => console.error("Here's your fetch error: ", err))
--- server.js
+++ server.js
@@ -1,65 +1,25 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const app = express()
-const port = 3000;
-require('dotenv').config()
-const mysql = require('mysql2')
-
+const app = express();
+exports.app = app;
+const { error } = require('console');
+const nodemailer = require(nodemailer);
 
 app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/contact", (req, res) => {
 
 
-const pool = mysql.createPool(
-    {
-        host: process.env.DB_host,
-        user: process.env.DB_user,
-        password: process.env.DB_pass,
-        database: process.env.DB_name,
-        port: process.env.DB_port,
-        waitForConnections: true,
-        connectionLimit: 10,
-        maxIdle: 10,
-        idleTimeout: 60000,
-        queueLimit: 0,
-        enableKeepAlive: true,
-        keepAliveInitialDelay: 0,
-    }
-)
-
-
-
-const connection = mysql.createConnection(
-    {
-        host: process.env.DB_host,
-        user: process.env.DB_user,
-        password: process.env.DB_pass,
-        database: process.env.DB_name,
-        port: process.env.DB_port
-    }
-)
-
-connection.query(
-    'SELECT * FROM users',
-    function (err, results, fields) {
-        if (results) {
-            console.log(results);
-            console.log(fields);
-        } else console.log(err)
-
-    }
-)
-
-
-
-app.get('/', (req, res) => {
-    res.send(`$500 dollars and $500 cents. And I got $500 sentences for you too bitch`)
-})
-
-app.get('/status', (req, res) => {
-    res.send("Hellow")
 })
 
 
 
-app.listen(port, console.log("Hello! Your app is running now!"));
 
+app.listen(3000, (err) => {
+    if (err) {
+        throw err
+    } else console.log("Hello! Your app is running now! " + 3000)
+});
