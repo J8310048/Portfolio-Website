9890480bc1dffa3016731c98c6de4657dab664d1
Joshua Brito-Infante <the.business.of.joshua@gmail.com>
2025-04-13 23:37:25 -0400

    added a nodemailer transport

--- server.js
+++ server.js
@@ -10,13 +10,28 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.post("/contact", (req, res) => {
 
 
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: process.env.EMAIL_host,
+    port: process.env.EMAIL_port,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_user,
+        pass: process.env.EMAIL_pass,
+    },
 })
 
 
+app.post("/contact", (req, res) => {
 
+    const { customerName, email, comment } = req.body
+
+    sendContactEmail(customerName, email, comment)
+    res.status(200).send("message successful!!!!!!!!!!!!!")
+
+})
 
 app.listen(3000, (err) => {
     if (err) {
